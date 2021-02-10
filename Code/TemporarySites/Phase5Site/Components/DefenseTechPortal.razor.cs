5a07cece438888713e2d9c76513764a571baaa90
Andy <musictopia2@hotmail.com>
2021-02-10 14:28:24 -0700

    Imported more images to improve design.

--- Code/TemporarySites/Phase5Site/Components/DefenseTechPortal.razor.cs
+++ Code/TemporarySites/Phase5Site/Components/DefenseTechPortal.razor.cs
@@ -1,21 +1,7 @@
-using Microsoft.AspNetCore.Components;
-using System;
-using System.Linq;
-using System.Net.Http;
-
 namespace Phase5Site.Components
 {
     public partial class DefenseTechPortal
     {
-        private readonly HttpClient Http;
-
-        public DefenseTechPortal()
-        {
-        }
-
-        public DefenseTechPortal(HttpClient httpClient) : this()
-        {
-            Http = httpClient;
-        }
+        
     }
 }
