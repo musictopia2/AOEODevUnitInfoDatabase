3ba0d4798d94ff464558684f474a89470ab3f450
Andy <musictopia2@hotmail.com>
2021-02-11 10:21:17 -0700

    Changed from server side to client side web assembly. Plus now its a progressive web application for this version.

--- Code/TemporarySites/Phase5Site/Program.cs
+++ Code/TemporarySites/Phase5Site/Program.cs
@@ -1,26 +1,33 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Phase5DataLibrary.Containers;
+using Phase5DataLibrary.Services;
+using Phase5DataLibrary.ViewModels;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
-
 namespace Phase5Site
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton<IUnitService, UnitService>(); //for now, this should be fine.
+            builder.Services.AddSingleton<ITechService, TechService>();
+            builder.Services.AddSingleton<IAnimationService, AnimationService>();
+            builder.Services.AddSingleton<IDamageExceptionService, DamageExceptionService>();
+            //can't do the unitviewmodel until something handles the stats.
+            //since this has to communicate with the stats now.
+            builder.Services.AddScoped<IUnitViewModel, UnitViewModel>();
+            builder.Services.AddScoped<IAttackResultsViewModel, AttackResultsViewModel>();
+            builder.Services.AddScoped<ICalculateUnitStatService, CalculateUnitStatService>();
+            builder.Services.AddScoped<TechListContainer, TechListContainer>();
+            builder.Services.AddScoped<ITechViewModel, TechViewModel>();
+            await builder.Build().RunAsync();
+        }
     }
 }
